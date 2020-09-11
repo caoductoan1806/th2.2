14a28c587b277ca2a31ecf018b00496a3ce115dd
ndt-pro <toannguyen080199@gmail.com>
2020-09-11 09:54:39 +0700

    update full

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { MainComponent } from './main/main.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SlideshowComponent,
+    MainComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule
